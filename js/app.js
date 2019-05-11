d03ca1e6feb34887b03a97be750f7f574dede242
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-11 10:36:36 +0300

    feat: printing the result

--- js/app.js
+++ js/app.js
@@ -24,5 +24,32 @@ function loadNames(e) {
     url += `amount=${amount}&`;
   }
 
-  console.log(url);
+  // Ajax Call
+  const xhr = new XMLHttpRequest();
+
+  // Open the connection
+  xhr.open("GET", url, true);
+
+  // Execute the function
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const names = JSON.parse(this.responseText);
+
+      // Insert intothe HTML
+      let html = "<h2>Generated Names</h2>";
+      html += '<ul class="list">';
+      names.forEach(function(name) {
+        html += `
+                <li>${name.name}</li>
+            `;
+      });
+
+      html += "</ul>";
+
+      document.querySelector("#result").innerHTML = html;
+    }
+  };
+
+  // Send the request
+  xhr.send();
 }
